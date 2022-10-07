9e94002396285c70e4a43e559e60a2cb2aafd5b5
rtexelm <texelist@gmail.com>
2022-10-07 13:58:36 -0400

    Completed phase 1 of longest contiguous subsum

--- time_differences_execution.rb
+++ time_differences_execution.rb
@@ -11,7 +11,7 @@ def my_min(arr)
   
 end
 
-p my_min([0,3,5,4,-5,10,1,90])
+# p my_min([0,3,5,4,-5,10,1,90])
 
 def my_min_faster(arr)
   arr.inject {|acc, ele| ele < acc ? ele : acc}
@@ -19,4 +19,23 @@ end
 
 p my_min_faster([0,3,5,4,-5,10,1,90])
 
+# arr.drop(i).inject
 
+def largest_contiguous_subsum(arr)
+  combo_arr = []
+  (0...arr.length).each do |i|
+    (i...arr.length).each do |j|
+      combo_arr << arr[i..j]
+    end
+  end
+  longest = combo_arr.inject do |acc, sub|
+    if acc.sum > sub.sum
+      acc
+    else
+      sub
+    end
+  end
+  longest.sum
+end
+
+p largest_contiguous_subsum([-5, -1, -3])
